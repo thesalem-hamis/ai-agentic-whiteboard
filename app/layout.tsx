9f50cf565a0bc180209fa33356a240e10a78d45b
Salem Hamis <140173982+salemhe@users.noreply.github.com>
2026-08-25 00:46:55 +0100

    Initial Commit

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import { ClerkProvider } from '@clerk/nextjs';
+import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Next.js Premium Startup Boilerplate",
+  description: "Created using the ultimate interactive Next.js stack generator CLI.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body style={{ margin: 0, padding: 0 }}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
